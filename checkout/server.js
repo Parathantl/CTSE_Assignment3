ed796af156fef6700fa9a874da6445ad558ad4fe
Deepika Srinivasan <IT19148632@my.sliit.lk>
2022-05-09 07:25:52 +0530

    Add docker file

--- checkout/server.js
+++ checkout/server.js
@@ -6,7 +6,7 @@ var bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
 
-const port = 3000;
+const port = 3001;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
