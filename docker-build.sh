b60436f3b4f4ed5aa07f2273c4128d706b11f50e
Parathan Thiyagalingam <parathanlive123@gmail.com>
2022-05-12 12:13:39 +0530

    sh script running cutomization

--- docker-build.sh
+++ docker-build.sh
@@ -1,12 +1,16 @@
 
 # docker build -t $1/nginx:$2 -t $1/nginx:latest .
 # docker push $1/nginx
-
+cd products
 docker build -t $1/products:$2 -t $1/products:latest .
 docker push -a $1/products
 
+cd ../checkout
 docker build -t $1/checkout:$2 -t $1/checkout:latest .
 docker push -a $1/checkout
 
+cd ../review
 docker build -t $1/review:$2 -t $1/review:latest .
 docker push -a $1/review
+
+cd ..
